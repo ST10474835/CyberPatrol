af187a437212f1dbdad2daacf78fea3460ae3797
Fanele <st10474835@rcconnect.edu.za>
2026-04-04 00:15:27 +0200

    Set up project structure and main files

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CyberPatrol
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+                Console.OutputEncoding = System.Text.Encoding.UTF8;
+                Console.Title = "CyberBot SA - Cybersecurity Awareness Assistant";
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.Clear();
+
+                DisplayHelper.ShowLogo();
+                AudioPlayer.PlayGreeting();
+
+            ConsoleUI.ShowLoadingBar();
+            ConsoleUI.ShowStartupSequence();
+            ConsoleUI.DrawDivider();
+
+                ChatBot bot = new ChatBot();
+                bot.Start();
+            }
+        }
+    }
+
+    
+
